5a4f34d757883c3600f5dbdf0f95c3bcd96d5148
AB <www.wpj@163.com>
2012-08-17 10:37:11 +0800

    修改消息弹出框样式

--- RemoteShutdown.Client(Udp)/Views/PopupMessageWindow.xaml.cs
+++ RemoteShutdown.Client(Udp)/Views/PopupMessageWindow.xaml.cs
@@ -18,7 +18,11 @@ namespace RemoteShutdown.Views
     /// </summary>
     public partial class PopupMessageWindow : Window
     {
-        private static double _count = 0d;
+        //private static double _count = 0d;
+        private static List<double> _indexes = new List<double>() { -1d };
+
+        private double _index = 0;
+
         private PopupMessageWindow()
         {
             InitializeComponent();
@@ -27,7 +31,7 @@ namespace RemoteShutdown.Views
         protected override void OnClosed(EventArgs e)
         {
             base.OnClosed(e);
-            _count--;
+            _indexes.Remove(_index);
         }
 
         public static void Show(string message)
@@ -36,16 +40,23 @@ namespace RemoteShutdown.Views
                 var window = new PopupMessageWindow();
                 window.Title = string.Format("服务器消息[{0:yy-M-d HH:mm:ss}]", DateTime.Now);
                 window.MessageTextBox.Text = message;
+                window._index = _indexes.Max() + 1;
+                _indexes.Add(window._index);
 
                 var maxVerticalCount = Math.Floor(SystemParameters.FullPrimaryScreenHeight / (window.Height + 10));
-                var top = SystemParameters.FullPrimaryScreenHeight - (_count % maxVerticalCount + 1d) * (window.Height + 10);
-                var left = SystemParameters.FullPrimaryScreenWidth - (Math.Floor(_count / maxVerticalCount) + 1d) * window.Width;
+                var top = SystemParameters.FullPrimaryScreenHeight - ((window._index) % maxVerticalCount + 1d) * (window.Height + 10);
+                var left = SystemParameters.FullPrimaryScreenWidth - (Math.Floor(window._index / maxVerticalCount) + 1d) * window.Width;
                 window.Left = left;
                 window.Top = top;
                 window.Show();
             }));
 
-            _count++;
+            //_count++;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
